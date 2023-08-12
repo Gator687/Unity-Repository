0aac45eac68986dcc23a7489e39a04fc180c1f9f
Gator687 <thomas.halligan68@gmail.com>
2023-08-12 01:45:44 -0400

    floating script

--- New Unity Project/Assets/Laterns and candles/scripts/floatingCandle.cs
+++ New Unity Project/Assets/Laterns and candles/scripts/floatingCandle.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class floatingCandle : MonoBehaviour
 {
+    public float floatHeight = 1.0f;       // Height at which the object floats
+    public float bobSpeed = 1.0f;          // Speed of bobbing
+    public float bobHeight = 0.1f;         // Amplitude of bobbing
+
+    private Vector3 originalPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Calculate the new y-position based on a sine wave to achieve bobbing effect
+        float newY = originalPosition.y + Mathf.Sin(Time.time * bobSpeed) * bobHeight;
+
+        // Set the new position of the object
+        transform.position = new Vector3(originalPosition.x, newY + floatHeight, originalPosition.z);
     }
 }
